d3bd941e9ac7afb677fac031636da0a44b71d00a
tanmay5268 <tanmay.mewati94@gmail.com>
2025-08-27 00:37:00 +0530

    Refactor code structure for improved readability and maintainability

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import servinAILogo from '../assets/servinai_logo.png'
+import { useNavigate } from 'react-router-dom'
+import { ArrowRight } from 'lucide-react'
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    return (
+        <div className='fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32'>
+            <img src={servinAILogo} alt="Servin.AI logo" className='w-32 sm:w-44 cursor-pointer' onClick={() => navigate('/')} />
+            <button
+                className="flex items-center gap-2 rounded-full text-sm cursor-pointer text-white px-10 py-2.5"
+                style={{ background: 'var(--color-primary)' }}
+            >
+                Get started <ArrowRight/>
+            </button>
+        </div>
+    )
+}
+
+export default Navbar
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../components/navbar' // Capitalized
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar /> 
+    </>
   )
 }
 
