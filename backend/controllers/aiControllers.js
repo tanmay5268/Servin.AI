d004d4a2244b65af1630b6ae2379bac014082e58
tanmay5268 <tanmay.mewati94@gmail.com>
2025-12-23 21:27:03 +0530

    feat: add image processing features and resume review functionality

--- backend/controllers/aiControllers.js
+++ backend/controllers/aiControllers.js
@@ -4,6 +4,8 @@ import sql from './../configs/db.js';
 import { GoogleGenAI } from "@google/genai";
 import axios from "axios";
 import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+import pdf from 'pdf-parse/lib/pdf-parse.js';
 const AI = new GoogleGenAI({
     apiKey: process.env.GOOGLE_API_KEY,
 
@@ -151,3 +153,157 @@ export const generateImage = async (req, res) => {
 }
 {/* ------------------------------------------------------------------------------------------------------------ */ }
 
+export const removeImageBackground = async (req, res) => {
+    try {
+        console.log('[CONTROLLER] Generate image start');
+        const userId = req.auth().userId; // Use from middleware instead of calling req.auth()
+        if (!userId) {
+            return res.status(401).json({ success: false, message: 'No user ID' });
+        }
+        const {image} = req.file;
+        /* const plan = req.plan;
+        const free_usage = req.free_usage;
+        console.log('[CONTROLLER] Params:', { userId, plan, free_usage, promptLength: prompt?.length });
+        
+        if (plan !== 'premium' && free_usage >= 10) {
+            return res.json({
+                success: false,
+                message: "Limit Reached. Upgrade to continue"
+            });
+        } */
+        
+        const {secure_url}=await cloudinary.uploader.upload(image.path, {
+            transformation: [{
+                effect: "background_removal",
+                backgrounf_removal: "remove_the_background"
+            }]
+        })
+
+
+        try { await sql`INSERT INTO creations (user_id,prompt,content,type) VALUES (${userId},'Remove backgrounf from image',${secure_url},'image')`; } catch (e) {
+            console.log('[CONTROLLER] DB Insert Error:', e.message);
+        }
+
+        /* if (plan !== 'premium') {
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: { free_usage: free_usage + 1 }
+            });
+        } */
+        res.json({ success: true, content:secure_url });
+    } catch (error) {
+        console.error('[CONTROLLER] Error:', error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+
+{/* ------------------------------------------------------------------------------------------------------------ */ }
+
+export const removeImageObject = async (req, res) => {
+    try {
+        console.log('[CONTROLLER] Generate image start');
+        const userId = req.auth().userId; // Use from middleware instead of calling req.auth()
+        if (!userId) {
+            return res.status(401).json({ success: false, message: 'No user ID' });
+        }
+        const {object}=req.body;
+        const {image} = req.file;
+        /* const plan = req.plan;
+        const free_usage = req.free_usage;
+        console.log('[CONTROLLER] Params:', { userId, plan, free_usage, promptLength: prompt?.length });
+        
+        if (plan !== 'premium' && free_usage >= 10) {
+            return res.json({
+                success: false,
+                message: "Limit Reached. Upgrade to continue"
+            });
+        } */
+        
+        const {public_id}=await cloudinary.uploader.upload(image.path)
+        const imageUrl=cloudinary.url(public_id, {
+            transformation:[{effect:`gen_remove:${object}`}],
+            resource_type: "image",
+        })
+
+        try { await sql`INSERT INTO creations (user_id,prompt,content,type) VALUES (${userId},${`Removed ${object} from image`},${imageUrl},'image')`; } catch (e) {
+            console.log('[CONTROLLER] DB Insert Error:', e.message);
+        }
+
+        /* if (plan !== 'premium') {
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: { free_usage: free_usage + 1 }
+            });
+        } */
+        res.json({ success: true, content:imageUrl });
+    } catch (error) {
+        console.error('[CONTROLLER] Error:', error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+
+
+export const resumeReview = async (req, res) => {
+    try {
+        console.log('[CONTROLLER] Generate image start');
+        const userId = req.auth().userId; // Use from middleware instead of calling req.auth()
+        if (!userId) {
+            return res.status(401).json({ success: false, message: 'No user ID' });
+        }
+        const resume = req.file;
+        /* const plan = req.plan;
+        const free_usage = req.free_usage;
+        console.log('[CONTROLLER] Params:', { userId, plan, free_usage, promptLength: prompt?.length });
+        
+        if (plan !== 'premium' && free_usage >= 10) {
+            return res.json({
+                success: false,
+                message: "Limit Reached. Upgrade to continue"
+            });
+        } */
+        
+        if(resume.size>5*1024*1024){
+            return  res.status(400).json({
+                success: false,
+                message: "File size should be less than 5MB"
+            });
+        }
+
+        const dataBuffer = fs.readFileSync(resume.path);
+        const pdf_data = await pdf(dataBuffer);
+        const prompt=`Review my resume and suggest improvements to make it more effective for job applications. Here is the content of my resume: \n\n${pdf_data.text}`;
+        
+        const response = await AI.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: prompt,
+            rules: [
+                "Do not include numbering in the titles.",
+                "Make sure the titles are engaging and relevant to the prompt."
+            ]
+        });
+        const content = response.text;
+
+        try { await sql`INSERT INTO creations (user_id,prompt,content,type) VALUES (${userId},'Review the uploaded resume',${content},'resume-review')`; } catch (e) {
+            console.log('[CONTROLLER] DB Insert Error:', e.message);
+        }
+
+        /* if (plan !== 'premium') {
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: { free_usage: free_usage + 1 }
+            });
+        } */
+        res.json({ success: true, content:content });
+    } catch (error) {
+        console.error('[CONTROLLER] Error:', error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { auth } from '../middlewares/auth.js';
-import { generateArticle, generateBlogTitle, generateImage } from '../controllers/aiControllers.js';
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from '../controllers/aiControllers.js';
+import upload from '../configs/multer.js';
 const aiRouter = express.Router();
 
 aiRouter.post('/generate-article',generateArticle)
 aiRouter.post('/generate-blog-title',generateBlogTitle)
 aiRouter.post('/generate-image', generateImage)
+aiRouter.post('/remove-image-background',upload.single('image') ,removeImageBackground)
+aiRouter.post('/remove-image-object',upload.single('image') ,removeImageObject)
+aiRouter.post('/resume-review',upload.single('resume') ,resumeReview)
 export default aiRouter;
